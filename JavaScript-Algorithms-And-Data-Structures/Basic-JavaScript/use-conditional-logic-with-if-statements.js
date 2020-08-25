fa50c0e026d6c58f697207e5c1325d7ad44d2451
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-08-25 23:53:40 +0530

    Add use-conditional-logic-with-if-statements.js

---
+++ JavaScript-Algorithms-And-Data-Structures/Basic-JavaScript/use-conditional-logic-with-if-statements.js
@@ -0,0 +1,8 @@
+function trueOrFalse(wasThatTrue) {
+  // Only change code below this line
+
+  return wasThatTrue? "Yes, that was true" : "No, that was false";
+
+  // Only change code above this line
+
+}
