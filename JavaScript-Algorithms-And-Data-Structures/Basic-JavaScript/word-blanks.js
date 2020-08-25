b787a85a1b60251313920559812aea85b8b9eb78
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-08-25 23:53:40 +0530

    Add word-blanks.js

---
+++ JavaScript-Algorithms-And-Data-Structures/Basic-JavaScript/word-blanks.js
@@ -0,0 +1,8 @@
+var myNoun = "dog";
+var myAdjective = "big";
+var myVerb = "ran";
+var myAdverb = "quickly";
+
+// Only change code below this line
+var wordBlanks = "My " + myNoun +" will "+ myVerb +" "+ myAdverb + " when it is " + myAdjective; // Change this line
+// Only change code above this line
