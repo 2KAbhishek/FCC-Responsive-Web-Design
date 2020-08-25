0da8e966c75866de022082f8afd41108950b8518
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-08-26 01:21:56 +0530

    Add add-new-properties-to-a-javascript-object.js

---
+++ JavaScript-Algorithms-And-Data-Structures/Basic-JavaScript/add-new-properties-to-a-javascript-object.js
@@ -0,0 +1,10 @@
+// Setup
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line
+myDog.bark = "woof";
