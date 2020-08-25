8f9fd853bb7245e7dda9a2b2a63ec08d3d2e5b64
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-08-25 23:53:40 +0530

    Add local-scope-and-functions.js

---
+++ JavaScript-Algorithms-And-Data-Structures/Basic-JavaScript/local-scope-and-functions.js
@@ -0,0 +1,12 @@
+function myLocalScope() {
+  'use strict';
+
+  // Only change code below this line
+  var myVar = 10;
+  console.log('inside myLocalScope', myVar);
+}
+myLocalScope();
+
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+console.log('outside myLocalScope', myVar);
