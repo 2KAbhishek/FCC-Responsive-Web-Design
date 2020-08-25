be6e5eca013e482c156f09765c0855bec0cdc215
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-08-25 23:53:41 +0530

    Add counting-cards.js

---
+++ JavaScript-Algorithms-And-Data-Structures/Basic-JavaScript/counting-cards.js
@@ -0,0 +1,26 @@
+var count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  switch(card){
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+    count++;
+    break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+    count--;
+    break;
+  }
+
+  return count > 0? count + " Bet": count + " Hold";
+  // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
