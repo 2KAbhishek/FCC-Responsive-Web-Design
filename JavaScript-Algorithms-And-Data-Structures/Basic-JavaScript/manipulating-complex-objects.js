f912e5f522b5fb64bf1fba98d7e0dac74df16c49
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-08-26 01:21:56 +0530

    Add manipulating-complex-objects.js

---
+++ JavaScript-Algorithms-And-Data-Structures/Basic-JavaScript/manipulating-complex-objects.js
@@ -0,0 +1,24 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  // Add a record here
+  {
+    "artist": "Radiohead",
+    "title": "Pablo Honey",
+    "release_year": 1992,
+    "formats": [
+      "CD",
+      "Digital"
+    ],
+    "gold": true
+  }
+];
