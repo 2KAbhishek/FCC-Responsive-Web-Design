0f9ade51395b9cc9e1733edfc37726137733c487
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-08-26 16:09:23 +0530

    Add use-recursion-to-create-a-countdown.js

---
+++ JavaScript-Algorithms-And-Data-Structures/Basic-JavaScript/use-recursion-to-create-a-countdown.js
@@ -0,0 +1,11 @@
+// Only change code below this line
+function countdown(n) {
+  if (n < 1) {
+    return [];
+  } else {
+    const countArray = countdown(n - 1);
+    countArray.unshift(n);
+    return countArray;
+  }
+}
+// Only change code above this line
