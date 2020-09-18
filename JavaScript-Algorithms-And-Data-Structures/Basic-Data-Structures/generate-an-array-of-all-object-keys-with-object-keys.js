20a1c307e58d9316b59b4b9685ab9f981c64c7d0
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-09-18 12:47:18 +0530

    Add generate-an-array-of-all-object-keys-with-object-keys.js

---
+++ JavaScript-Algorithms-And-Data-Structures/Basic-Data-Structures/generate-an-array-of-all-object-keys-with-object-keys.js
@@ -0,0 +1,27 @@
+let users = {
+  Alan: {
+    age: 27,
+    online: false
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: false
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+
+function getArrayOfUsers(obj) {
+  // Only change code below this line
+  let arr = Object.keys(obj);
+  return arr;
+  // Only change code above this line
+}
+
+console.log(getArrayOfUsers(users));
