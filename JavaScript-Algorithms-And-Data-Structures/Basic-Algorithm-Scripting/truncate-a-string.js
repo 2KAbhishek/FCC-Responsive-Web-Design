009f85ec9abf5e87b90d3642f0b12fef65b9c048
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-09-21 23:42:33 +0530

    Add truncate-a-string.js

---
+++ JavaScript-Algorithms-And-Data-Structures/Basic-Algorithm-Scripting/truncate-a-string.js
@@ -0,0 +1,6 @@
+function truncateString(str, num) {
+  // return str.length > num ? str.slice(0,num) + "..." : str;
+  return str.length > num ? str.substr(0,num) + "..." : str;
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
