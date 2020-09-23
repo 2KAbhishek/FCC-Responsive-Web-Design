1648c9a233d5ae2f8cc22ee060ebf7e2f276c68a
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-09-23 09:33:21 +0530

    Add use-an-iife-to-create-a-module.js

---
+++ JavaScript-Algorithms-And-Data-Structures/Object-Oriented-Programming/use-an-iife-to-create-a-module.js
@@ -0,0 +1,15 @@
+let funModule = (function () {
+  return {
+    isCuteMixin: function(obj) {
+      obj.isCute = function() {
+       return true;
+      };
+    },
+    singMixin: function(obj) {
+     obj.sing = function() {
+      console.log("Singing to an awesome tune");
+     };
+   }
+  }
+})();
+
