29ccab9a39bb1b972a9170e2ad2d1cb0be8f4e3e
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-09-24 23:57:19 +0530

    Add return-a-sorted-array-without-changing-the-original-array.js

---
+++ JavaScript-Algorithms-And-Data-Structures/Functional-Programming/return-a-sorted-array-without-changing-the-original-array.js
@@ -0,0 +1,8 @@
+var globalArray = [5, 6, 3, 2, 9];
+function nonMutatingSort(arr) {
+  // Only change code below this line
+  return arr.concat([]).sort();
+
+  // Only change code above this line
+}
+nonMutatingSort(globalArray);
