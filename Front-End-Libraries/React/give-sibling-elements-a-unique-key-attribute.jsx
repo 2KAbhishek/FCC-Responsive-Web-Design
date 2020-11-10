2ab77d7df7a37c3f30def7713e1ced8be32a2cda
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-11-10 14:50:31 +0530

    Add give-sibling-elements-a-unique-key-attribute.jsx

---
+++ Front-End-Libraries/React/give-sibling-elements-a-unique-key-attribute.jsx
@@ -0,0 +1,22 @@
+const frontEndFrameworks = [
+  'React',
+  'Angular',
+  'Ember',
+  'Knockout',
+  'Backbone',
+  'Vue'
+];
+
+function Frameworks() {
+  const renderFrameworks = frontEndFrameworks.map((item) =>
+  <li key={item+1}>{item}</li>
+);; // Change this line
+  return (
+    <div>
+      <h1>Popular Front End JavaScript Frameworks</h1>
+      <ul>
+        {renderFrameworks}
+      </ul>
+    </div>
+  );
+};
