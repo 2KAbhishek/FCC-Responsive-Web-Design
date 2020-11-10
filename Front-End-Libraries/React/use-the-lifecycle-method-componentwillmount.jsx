3e550434eeeb095a7cccbf532023d3edac40b6ac
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-11-10 14:50:31 +0530

    Add use-the-lifecycle-method-componentwillmount.jsx

---
+++ Front-End-Libraries/React/use-the-lifecycle-method-componentwillmount.jsx
@@ -0,0 +1,13 @@
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentWillMount() {
+    // Change code below this line
+    console.log('componentWillMount is removed');
+    // Change code above this line
+  }
+  render() {
+    return <div />
+  }
+};
