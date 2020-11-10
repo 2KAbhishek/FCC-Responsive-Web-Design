ab6d9c3209ecc5fd581e4dd6611ec3e623ad7312
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-11-10 14:50:30 +0530

    Add create-a-complex-jsx-element.jsx

---
+++ Front-End-Libraries/React/create-a-complex-jsx-element.jsx
@@ -0,0 +1,11 @@
+const JSX = (
+    <div>
+    <h1> Hello JSX! </h1>
+    <p> JSX Paragraph </p>
+    <ul>
+        <li> Item 1 </li>
+        <li> Item 2 </li>
+        <li> Item 3 </li>
+    </ul>
+    </div>
+)
