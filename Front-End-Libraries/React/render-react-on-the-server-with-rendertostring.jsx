9c0e6784ee9b5487e0237f02a613f316ff92a909
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-11-10 14:50:31 +0530

    Add render-react-on-the-server-with-rendertostring.jsx

---
+++ Front-End-Libraries/React/render-react-on-the-server-with-rendertostring.jsx
@@ -0,0 +1,11 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <div/>
+  }
+};
+
+// Change code below this line
+ReactDOMServer.renderToString(<App/>);
