e70fdaa2fbe84a4c7bf12e6a53d62923fdd0b1d7
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-12-27 19:36:06 +0530

    Add write-a-counter-with-redux.js

---
+++ Front-End-Libraries/Redux/write-a-counter-with-redux.js
@@ -0,0 +1,23 @@
+const INCREMENT = "INCREMENT"; // Define a constant for increment action types
+const DECREMENT = "DECREMENT"; // Define a constant for decrement action types
+
+const counterReducer = (state = 0, action) => {
+    switch (action.type) {
+        case INCREMENT:
+        return state += 1
+        case DECREMENT:
+        return state -= 1
+        default:
+        return state;
+    }
+}; // Define the counter reducer which will increment or decrement the state based on the action it receives
+
+const incAction = () => {
+    return {type: INCREMENT}
+}; // Define an action creator for incrementing
+
+const decAction = () => {
+    return {type: DECREMENT}
+}; // Define an action creator for decrementing
+
+const store = Redux.createStore(counterReducer); // Define the Redux store here, passing in your reducers
