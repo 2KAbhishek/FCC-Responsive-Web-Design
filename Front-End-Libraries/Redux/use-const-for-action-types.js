0551317ba16895a66e7b112b777771ca629c67c8
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-12-27 19:36:00 +0530

    Add use-const-for-action-types.js

---
+++ Front-End-Libraries/Redux/use-const-for-action-types.js
@@ -0,0 +1,43 @@
+// Change code below this line
+const LOGIN = 'LOGIN';
+const LOGOUT = 'LOGOUT';
+// Change code above this line
+
+const defaultState = {
+  authenticated: false
+};
+
+const authReducer = (state = defaultState, action) => {
+
+  switch (action.type) {
+
+    case LOGIN:
+      return {
+        authenticated: true
+      }
+
+    case LOGOUT:
+      return {
+        authenticated: false
+      }
+
+    default:
+      return state;
+
+  }
+
+};
+
+const store = Redux.createStore(authReducer);
+
+const loginUser = () => {
+  return {
+    type: LOGIN
+  }
+};
+
+const logoutUser = () => {
+  return {
+    type: LOGOUT
+  }
+};
